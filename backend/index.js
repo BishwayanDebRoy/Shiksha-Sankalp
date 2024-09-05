f477072e28497f776cc7aa0be47590493ecfb42c
BishwayanDebRoy <bb3546@srmist.edu.in>
2024-09-05 12:50:41 +0530

    mongodb URI is added

--- backend/index.js
+++ backend/index.js
@@ -16,17 +16,23 @@ require('dotenv/config')
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost'],
+    methods: "GET, POST,",
+    credentials: true,
+
+
+}));
 
 
 
-    mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        dbName: 'cfg'
-      })
-      .then(() => console.log('Database connection established.'))
-      .catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'cfg'
+})
+    .then(() => console.log('Database connection established.'))
+    .catch(err => console.error(err));
 
 app.use('/', Routes);
 
